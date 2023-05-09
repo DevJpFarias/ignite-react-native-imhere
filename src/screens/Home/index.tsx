0682ac9eac7cfc765e9e2c0b300f869291685595
João <joaopfc2764@gmail.com>
2023-05-09 12:50:32 -0300

    create data input

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,7 +1,7 @@
-import { Text, View } from 'react-native';
+import { Text, View, TextInput } from 'react-native';
 import { styles } from './styles';
 
-export default function Home() {
+export function Home() {
   return (
     <View style={styles.container}>
       <Text key="1" style={styles.eventName}>
@@ -11,6 +11,12 @@ export default function Home() {
       <Text style={styles.eventDate}>
         Sexta, 9 de Novembro de 2023.
       </Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder='Nome do participante'
+        placeholderTextColor='#6b6b6b'
+      />
     </View>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -15,5 +15,14 @@ export const styles = StyleSheet.create({
   eventDate: {
     color: '#6b6b6b',
     fontSize: 16
+  },
+  input: {
+    height: 56,
+    backgroundColor: '#1f1e25',
+    borderRadius: 5,
+    color: '#fff',
+    padding: 16,
+    fontSize: 16,
+    marginTop: 10,
   }
 })
