8bd6bf3eb30827615bf01b3f4c4f7a8999f94ccf
João <joaopfc2764@gmail.com>
2023-05-16 01:24:37 -0300

    create participants list with create and delete

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,9 +1,32 @@
-import { Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { Text, View, TextInput, TouchableOpacity, FlatList } from 'react-native';
 import { styles } from './styles';
 
+import { Participant } from '../../components/Participant';
+
 export function Home() {
+  const participants = [
+    'João Paulo',
+    'Ludmila',
+    'Lorena',
+    'Ana Clara',
+    'Ana Sodré',
+    'Gabriel Antunes',
+    'Daniel Martins',
+    'Anna Victoria',
+    'Stefane',
+    'Felipe',
+    'Amanda',
+    'Thais',
+    'Amanda Fagundes',
+    'Gabriel',
+  ]
+
   function handleParticipantAdd() {
     console.log('Você clicou no botão de adicionar')
+  }
+
+  function handleParticipantRemove(name: string) {
+    console.log(`Você clicou no botão de remover participante ${name}`)
   }
 
   return (
@@ -27,6 +50,31 @@ export function Home() {
           <Text style={styles.buttonText}>+</Text>
         </TouchableOpacity>
       </View>
+
+      <FlatList
+        data={participants}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Participant
+            key={item}
+            name={item}
+            onRemove={() => handleParticipantRemove(item)}
+          />
+        )}
+        showsVerticalScrollIndicator={false}
+        ListEmptyComponent={() => (
+          <Text style={styles.listEmptyText}>Ninguém chegou no evento ainda? Adicione participantes!</Text>
+        )}
+      />
+      {/* {
+        participants.map(participant => (
+          <Participant
+            key={participant}
+            name={participant}
+            onRemove={() => handleParticipantRemove(participant)}
+          />
+        ))
+      } */}
     </View>
   );
 }
